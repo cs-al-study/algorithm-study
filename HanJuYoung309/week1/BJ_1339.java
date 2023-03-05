e13d6f6e4917b24594773ef01ec862a01dedf9ae
HanJuYoung309 <juyg407@naver.com>
2023-03-06 00:21:22 +0900

    Create BJ_1339.java
    백준1339

---
+++ HanJuYoung309/week1/BJ_1339.java
@@ -0,0 +1,35 @@
+package backJoon;
+
+import java.util.Scanner;
+
+public class BJ_1339 {
+
+    public static void main(String[] args) {
+
+
+        Scanner sc= new Scanner(System.in);
+
+        int max=Integer.MAX_VALUE;
+        //1.단어 개수 입력받기
+        int n= sc.nextInt();
+
+        //숫자 저장배열
+        int num[]= new int[9];
+
+        //알파벳 저장 배열
+        char alphbat[]= new char[10];
+
+        //반복할 배열
+        String str[]= new String[n];
+
+        for(int i=0; i<str.length; i++){
+            str[i]=sc.nextLine();
+        }
+
+        //swap
+
+
+
+
+    }
+}
