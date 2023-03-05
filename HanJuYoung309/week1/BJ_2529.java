93e0ae4b6bb1c5c621c06f8962fe8eb46a1112ec
HanJuYoung309 <juyg407@naver.com>
2023-03-05 22:40:07 +0900

    Update BJ_2529.java
    커밋 테스트

--- HanJuYoung309/week1/BJ_2529.java
+++ HanJuYoung309/week1/BJ_2529.java
@@ -22,14 +22,13 @@ public class BJ_2529 {
 
             data[i]=sc.nextLine();
 
-
-
         }
 
         //k개의 부등호순서를 만족하는 k+1자리의 정수중 최대/최소
 
         System.out.println("두번째커밋 테스트...");
         System.out.println("세번째커밋 테스트");
+        System.out.println("네번째 커밋 테스트...");
 
 
     }
